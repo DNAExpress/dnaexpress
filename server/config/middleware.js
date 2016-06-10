35122cb3ca1e476db048a2829c996d79e3d5df10
Diana <dianajohnson13@gmail.com>
2016-06-09 19:25:34 -0700

    [feat] add routing for /search requests

--- server/config/middleware.js
+++ server/config/middleware.js
@@ -2,11 +2,17 @@ var bodyParser = require('body-parser');
 
 
 module.exports = function(app, express) {
+  var searchRouter = express.Router();
+  // var userRouter = express.Router();
+  // var eventRouter = express.Router();
 
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({extended:false}));
 
   app.use(express.static(__dirname + '/../../client'));
 
+  app.use('/api/search', searchRouter);
+
+  require('../search/search_router.js')(searchRouter);
 
 };
