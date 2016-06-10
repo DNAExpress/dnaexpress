763b2bb4c2f0d86da3181a7f33b67dcc05ce6317
Nate Blain <blain89@gmail.com>
2016-06-10 10:41:38 -0700

    [fix] uncomment

--- server/data/db_schema.js
+++ server/data/db_schema.js
@@ -1,28 +1,28 @@
 
-// var mongoose = require('mongoose');
-// var options = { server: { socketOptions: { connectTimeoutMS: 1000000, maxTimeMS:100000 }}};
-// var databaseUrl = 'mongodb://localhost/server/data/db/';
-// mongoose.connect('mongodb://localhost/server/data/db/');
+var mongoose = require('mongoose');
+var options = { server: { socketOptions: { connectTimeoutMS: 1000000, maxTimeMS:100000 }}};
+var databaseUrl = 'mongodb://localhost/server/data/db/';
+mongoose.connect('mongodb://localhost/server/data/db/');
 
-// var db = mongoose.connection;
+var db = mongoose.connection;
 
-// db.on('error', console.error.bind(console, "My own Connection Error"));
-// db.once('open', function() {
-//   console.log("Connection to localhost:8000/server/data/db is open")
-// });
+db.on('error', console.error.bind(console, "My own Connection Error"));
+db.once('open', function() {
+  console.log("Connection to localhost:8000/server/data/db is open")
+});
 
-// var userSchema = mongoose.Schema({
-//   name:String
-// });
+var userSchema = mongoose.Schema({
+  name:String
+});
 
-// var User = mongoose.model('User', userSchema);
+var User = mongoose.model('User', userSchema);
 
-// var nate = new User({name:'Nate'});
+var nate = new User({name:'Nate'});
 
-// nate.save(function(err, nate) {
-//   if (err) {
-//     console.log("Error inside save", err)
-//   }
-//   console.log(nate.name)
-// });
-// module.exports = db;
+nate.save(function(err, nate) {
+  if (err) {
+    console.log("Error inside save", err)
+  }
+  console.log(nate.name)
+});
+module.exports = db;
--- server/server.js
+++ server/server.js
@@ -6,4 +6,4 @@ require('./config/middleware.js')(app, express);
 
 module.exports = app;
 
-// var db = require('./data/db_schema.js');  //temporary to get the database to initialize
+var db = require('./data/db_schema.js');  //temporary to get the database to initialize
