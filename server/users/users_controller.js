29c0a190338e3f6cf507e1f5cc6b998977bb1d22
Diana <dianajohnson13@gmail.com>
2016-06-11 12:07:15 -0700

    [feat] build out logic for user auth; require jwt-simple for tokening

--- server/users/users_controller.js
+++ server/users/users_controller.js
@@ -1,9 +1,22 @@
-var usersModel = require('./users_model.js');
+var Users = require('./users_model.js');
+var jwt = require('jwt-simple');
 
 module.exports = userControls = {
+
   signup: function signup(req, res) {
-    // do something
-    console.log('attempting to add user')
+    console.log('in users_controller.js: attempting to signup user')
+
+    var email: req.body.email;
+    var password: req.body.password;
+
+    Users.findOne({'email': email})
+      .then(function(user) {
+        if (user) {
+          // res that that user already exits 
+        } else {
+          // add user to db - encrypt password, send back token
+        }
+      })
     // check if user exists in db
       // if the user does not exist
         // add them
@@ -12,14 +25,30 @@ module.exports = userControls = {
   },
 
   signin: function signin(req, res) {
-
+    console.log('in users_controller.js: attempting to signin user')
+    var email: req.body.email;
+    var password: req.body.password;
+    
+    Users.findOne({'email': email})
+      .then(function(user) {
+        if (user) {
+          // check password...
+            // if passwork checks out
+              // make token
+              // res with success and token
+        } else {
+          // res that user does not exist
+        }
+      })
   },
 
   checkAuth: function checkAuth(req, res) {
-
+    console.log('in users_controller.js: attempting to checkAuth')
+    // 
   },
 
   editUserProfile: function(req, res) {
+    console.log('in users_controller.js: attempting to edit profile')
     // authenticate first
 
     // use for adding and updating a users profile?
