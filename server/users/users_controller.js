d62f0d825ced869b202606f796b5314b5d4ef6ea
Diana <dianajohnson13@gmail.com>
2016-06-10 17:25:15 -0700

    [feat] add users controller for handling user related reqs

---
+++ server/users/users_controller.js
@@ -0,0 +1,29 @@
+var usersModel = require('./users_model.js');
+
+module.exports = userControls = {
+  signup = function signup(req, res) {
+    // do something
+    console.log('attempting to add user')
+    // check if user exists in db
+      // if the user does not exist
+        // add them
+        // res with token
+      // if they already exist, res with that info
+
+  },
+
+  signin = function signin(req, res){
+
+  },
+
+  editUserProfile = function(req, res) {
+    // authenticate first
+
+    // use for adding and updating a users profile?
+    // or redirect to a function for each
+  },
+
+  // handle connections - requestConnection, acceptConnection, delete connection
+
+  // signin, signout / authentication
+};
