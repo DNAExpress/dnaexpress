6e8f7df0631409271fc7a691dd4ea5754fa9b4c9
Nate Blain <blain89@gmail.com>
2016-06-10 19:46:52 -0700

    [chore] change server to server root route; implement restaurant results

--- client/app/controllers/restaurantResults.js
+++ client/app/controllers/restaurantResults.js
@@ -4,5 +4,12 @@ angular.module('app.restaurantresults', [])
   $scope.data = {
     restaurants: restaurantFactory.restaurants
   }
+<<<<<<< f7963223e3f192ccc2fd821907d42c3c7bf1a752
+=======
+
+  $scope.logger = function() {
+    console.log('hit');
+  }
+>>>>>>> [chore] change server to server root route; implement restaurant results
 
 }]);
