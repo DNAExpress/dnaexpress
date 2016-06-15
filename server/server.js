21200ef7ef7658adadc600d35866504c8a0baf96
Diana <dianajohnson13@gmail.com>
2016-06-15 14:44:40 -0700

    [chore] remove unused ref to db in server.js

--- server/server.js
+++ server/server.js
@@ -3,7 +3,4 @@ var app = express();
 
 require('./config/middleware.js')(app, express);
 
-
 module.exports = app;
-
-var db = require('./data/db_schema.js');  //temporary to get the database to initialize
