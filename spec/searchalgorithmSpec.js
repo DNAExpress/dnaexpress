988106c15670fa23614497d5a293dddaaf08947d
Nate Blain <blain89@gmail.com>
2016-06-15 16:02:14 -0700

    [chore] add tests for search algorithm

--- spec/searchalgorithmSpec.js
+++
@@ -1,8 +0,0 @@
-require('../server/search/searchalgorithm');
-
-describe('Search Algorithm', function(){
-
-  it('Should use a function called totalUserPrefs', function(){
-    expect(totalUserPrefs).toBeDefined();
-  });
-});
--- spec/server/search_algorithm_Spec.js
+++ spec/server/search_algorithm_Spec.js
@@ -1,8 +1,48 @@
-require('../../server/search/searchalgorithm');
+var searchAlgorithm = require('../../server/search/searchalgorithm');
+var totalUserPrefs = searchAlgorithm.totalUserPrefs;
+var histogram = searchAlgorithm.histogram;
+var threeHeighest = searchAlgorithm.threeHeighest;
+var percentage = searchAlgorithm.percentage;
+var parseBestOptions = searchAlgorithm.parseBestOptions;
 
-describe('Search Algorithm', function(){
+describe('totalUserPrefs', function(){
 
-  it('Should use a function called totalUserPrefs', function(){
+  it('Should be a function', function(){
     expect(totalUserPrefs).toBeDefined();
   });
+
+  // it ('Should ')
+
+});
+
+describe('histogram', function(){
+
+  it('Should be a function', function(){
+    expect(histogram).toBeDefined();
+  });
+
+});
+
+describe('threeHeighest', function(){
+
+  it('Should be a function', function(){
+    expect(threeHeighest).toBeDefined();
+  });
+
+});
+
+describe('percentage', function(){
+
+  it('Should be a function', function(){
+    expect(percentage).toBeDefined();
+  });
+
+});
+
+describe('parseBestOptions', function(){
+
+  it('Should be a function', function(){
+    expect(parseBestOptions).toBeDefined();
+  });
+
 });
