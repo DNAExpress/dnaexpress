4ab415d5bba7cce9b707127d21a4ed7fe9ca2b56
Nate Blain <blain89@gmail.com>
2016-06-14 20:00:27 -0700

    [feat] implement search algorithm and started specs for algorithm

--- server/search/searchalgorithm.js
+++ server/search/searchalgorithm.js
@@ -2,11 +2,23 @@
 // algorithm for most likely preferences
   // for each array of arrays
     // for each array
-  // individual
+  // individual [[[person1Profile], [person1Current]], [[person1Profile], [person1Current]], [[person1Profile], [person1Current]]]
   'use strict'
-  var profilePrefs = ['steak', 'salad', 'sushi'];
-  var currentPrefs = ['steak', 'salad', 'sushi'];
-  var numOfPeople = 2;
+  var users = [[['a', 'b', 'c'], ['a', 'd', 'e']], [[['a', 'b', 'y']], ['p', 'y', 'x']], [['a', 'r', 'b'], ['a', 't', 'v']]];
+  var numOfPeople = users.length;
+
+  // function to pull each profile and current from schema and add as an array to the results array - this will be passed into parseBestOptions
+  function totalUserPrefs(userPrefs) { //expects an array of both profile and current choices
+    var result = [];
+    console.log(userPrefs);
+    userPrefs.forEach(function(item){
+      if (result.indexOf(item) !== -1) {
+        result.push(item);
+      }
+    });
+    return result; // this is the total different foods of a single user with repeats parsed out
+  }
+
   function histogram(foodprefs) {
     var result = {};
     foodprefs.forEach(function(food){
@@ -16,47 +28,78 @@
         result[food]++;
       }
     });
+    console.log(result);
     return result;
   }
-  //individual
-  function filterFoods(obj) {
-    var newObj = {};
-    obj.forEach(function(item) {
-      if (obj[key] === 2) {
-        newObj[key] = 2;
-      }
-    });
-    if (Object.keys(newObj).length === 0) {
-      return currentPrefs;
-    } else {
-      return Object.keys(newObj);
+
+  function threeHeighest(obj, userNum) { // get the most frequently chosen foods
+    var result = [];
+    while (result.length < 3) {
+      var high = _.reduce(Object.keys(obj), function(a, b){ return obj[a] > obj[b] ? a : b; });
+      var temp = [high, obj[high]];
+      delete obj[high];
+      result.push(temp);
     }
+    return result;
   };
 
-  function parseBestOptions(array) {
-    var flat;
-    Array.isArray(array[0]) ? flat = _.flatten(array) : flat = array;
-    var totalFoods = histogram(flat);
+  function percentage(choiceNum, personNum) {
+    return ((choiceNum*100)/personNum).toFixed(2);
+  }
 
+
+  function parseBestOptions(allUserChoices) {
+    var allUserPrefs = allUserChoices.map(function(user){
+      return totalUserPrefs(user[0].concat(user[1]));
+    });
+    return threeHeighest(histogram(_.flatten(allUserPrefs))); // number of people for each specified choices for all users
   }
-// in the works
-  function highestCounts(foodprefs) {
-    // var counts = [];
-    // iterate over object
-      //
-    var results = [];
-    for (var key in foodprefs) {
-      results.push([key, foodprefs[key]]);
-    }
-    var highestThree = counts.sort().slice(-3);
-    for (var key in foodprefs) {
-      highestThree.map(function(item){
-        if (foodprefs[key] === item) {
-          results.push()
-        }
-      })
-    }
-  }
+
+  //totalUserPrefs();
+  parseBestOptions(users);
+
+  // function filterFoods(obj, curprefs) {
+  //   var newObj = {};
+  //   for(var key in obj) {
+  //     if (obj[key] === 2) {
+  //       newObj[key] = 2;
+  //     }
+  //   };
+
+
+
+
+//     if (Object.keys(newObj).length === 0) {
+//       return curprefs;  // if there are no current choices that match profile return the current choices
+//     } else {
+//       return Object.keys(newObj); // return current choices that match profile choices
+//     }
+//   };
+
+//   // function parseBestOptions(array) {
+//     var flat;
+//     Array.isArray(array[0]) ? flat = _.flatten(array) : flat = array;
+//     var totalFoods = histogram(flat);
+
+//   // }
+// // in the works
+//   function highestCounts(foodprefs) {
+//     // var counts = [];
+//     // iterate over object
+//       //
+//     var results = [];
+//     for (var key in foodprefs) {
+//       results.push([key, foodprefs[key]]);
+//     }
+//     var highestThree = counts.sort().slice(-3);
+//     for (var key in foodprefs) {
+//       highestThree.map(function(item){
+//         if (foodprefs[key] === item) {
+//           results.push()
+//         }
+//       });
+//     }
+//   }
 
   // function comparePrefs(foodPrefObj) {
   //   var result;
---
+++ spec/server/search_algorithm_Spec.js
@@ -0,0 +1,8 @@
+require('../../server/search/searchalgorithm');
+
+describe('Search Algorithm', function(){
+
+  it('Should use a function called totalUserPrefs', function(){
+    expect(totalUserPrefs).toBeDefined();
+  });
+});
