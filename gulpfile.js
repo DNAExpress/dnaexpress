7c530d6af3944719dbf338d1656f1d736a6cfbc7
Nate Blain <blain89@gmail.com>
2016-06-15 20:08:53 -0700

    [chore] work on build files

--- gulpfile.js
+++ gulpfile.js
@@ -3,40 +3,41 @@ var gulp = require('gulp'),
   jshint = require('gulp-jshint'),
   jasmine = require('gulp-jasmine'),
   uglify = require('gulp-uglify'),
-  rename = require('gulp-rename')
+  rename = require('gulp-rename'),
+  ngInject = require('gulp-ng-inject'),
   notify = require('gulp-notify')
 
-  gulp.task('jasmine-tests', function () {
+  gulp.task('test', function () {
     gulp.src('spec/**/*.js')
-      .pipe(jasmine());
+      .pipe(jasmine())
+      .pipe(notify({message: 'Finished testing!'}));
   });
 
-  gulp.task('concat', function () {});
+  gulp.task('lint', function() {
+    return gulp.src('client/app/**/*.js')
+      .pipe(jshint())
+      .pipe(notify({message: 'Linting complete!'}));
+  });
 
   gulp.task('client-scripts', function () {
-    gulp.src('client/app/**/*.js')
+
+    var options = {
+        name: "ng-inject", // The name of the module to use in your main Angular.js
+        modules: ['ui.router'] // Any extra modules that you want to include.
+    };
+
+    return gulp.src(['client/app/**/*.js', 'client/app.js'])
       .pipe(jshint())
-      .pipe(concat('client-main.js'))
+      .pipe(ngInject("client-main.js", options))
       .pipe(rename({suffix: '.min'}))
       .pipe(uglify())
-      .pipe(gulp.dest('build/js'))
+      .pipe(gulp.dest('client/dist'))
       .pipe(notify({message: 'Task: client-scripts complete!'}));
   });
 
-  gulp.task('server-scripts', function () {
-    gulp.src('server/**/*.js')
-      .pipe(jshint())
-      .pipe(concat('server-main.js'))
-      .pipe(rename({suffix: '.min'}))
-      .pipe(uglify())
-      .pipe(gulp.dest('build/js'))
-      .pipe(notify({message: 'Task: server-scripts complete!'}));
-  });
-
-  gulp.task('default', ['client-scripts', 'server-scripts', 'jasmine-tests']);
+  gulp.task('default', ['client-scripts', 'jasmine-tests']);
 
   gulp.task('watch', function () {
     gulp.watch('client/app/**/*.js', ['client-scripts']);
-    gulp.watch('server/**/*.js', ['server-scripts'])
   });
 
