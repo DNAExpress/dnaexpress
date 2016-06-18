a772253f829cab52e5ce2c036a529e24c1336337
Nate Blain <blain89@gmail.com>
2016-06-17 19:37:52 -0700

    [feat] add custom id generator

--- server/data/models/event.js
+++ server/data/models/event.js
@@ -1,6 +1,7 @@
 var db = require('./../db_schema.js');
 var User = require('./user');
-// var Suggestions = require('./event_suggestions')
+var Recommendation = require('./recommendation');
+
 var Event = db.Model.extend({
   tableName: 'events',
   hasTimestamp: true,
@@ -8,18 +9,34 @@ var Event = db.Model.extend({
     return this.belongsToMany(User, 'usersEvents');
     //through usersEvents
   },
-  // suggestions: function() {
-  //   return this.hasMany('Suggestions');
-  // },
+  createCustomId: function () {
+    var num = [1,2,3,4,5,6,7,8,9,'a', 'b', 'c', 'd', 'e'];
+
+    return function(){
+      var i = 0
+        , j = 0
+        , temp = null;
+
+      for (i = num.length - 1; i > 0; i -= 1) {
+        j = Math.floor(Math.random() * (i + 1));
+        temp = num[i];
+        num[i] = num[j];
+        num[j] = temp;
+      }
+      this.set('publicEventId', num.join(''));
+      console.log('initializing event');
+    }().bind(this);
+  },
+  recommendation: function() {
+    return this.hasMany(Recommendation);
+  },
 
   creator: function() {
-    return this.hasOne('User');
+    return this.hasOne(User);
   },
 
   initialize: function() {
-    return this.on('creating', function() {
-      console.log('initializing event')
-    });
+    return this.on('creating', this.createCustomId());
   },
 });
 
