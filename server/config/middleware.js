cabcfa37919aeb746e681e4bba28daec2bc41604
Nate Blain <blain89@gmail.com>
2016-06-17 19:32:08 -0700

    [feat] create routes for events

--- server/config/middleware.js
+++ server/config/middleware.js
@@ -4,7 +4,7 @@ var fs = require('fs');
 module.exports = function(app, express) {
   var searchRouter = express.Router();
   var usersRouter = express.Router();
-  // var eventRouter = express.Router();
+  var eventsRouter = express.Router();
 
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({extended:false}));
@@ -12,10 +12,13 @@ module.exports = function(app, express) {
   app.use(express.static(__dirname + '/../../client'));
 
   app.use('/api/search', searchRouter);
-  app.use('/api/users', usersRouter)
+  app.use('/api/users', usersRouter);
+
+  app.use('/api/events', eventsRouter);
 
   require('../search/search_router.js')(searchRouter);
   require('../users/users_router.js')(usersRouter);
+  require('../events/events_router.js')(eventsRouter);
 
   app.use(function errorHandler(error, req, res, next) {
     res.status(500).send({error: error.message});
