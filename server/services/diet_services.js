8fac37098800ef5e7a87baa2577f04a4dd8566c6
Nate Blain <blain89@gmail.com>
2016-06-21 16:04:14 -0700

    [chore] implement diet services methods

--- server/services/diet_services.js
+++ server/services/diet_services.js
@@ -1,73 +1,115 @@
-// var Bookshelf = require('./../db_schema.js');
 var DietRestrictions = require('./../data/collections/diet_restrictions');
 var DietRestriction = require('./../data/models/diet_restrictions');
 var Users = require('./../data/collections/users');
 var User = require('./../data/models/user');
 
 module.exports = dietServices = {
-  getDietRestrictions: function () {
-    // var dietRestriction = new DietRestriction({});
-  },
-  editDietRestrictions: function editFoodRestrictions (user, userRestrictions) {
-    // fetch users pre-existing restrictions
-      // if a restriction has been removed, remove their connection with it from the db
-      // if one has been added, add their connection with it to the db
-      // var totalRestrictions = [];
-      // userRestrictions.forEach(function(restriction){
-  },
-  addDietRestriction: function (userid, userRestrictions) {
-    // userRestrictions.forEach(function (restriction) {
-    //   var restriction_id = bookshelf.knex.raw("SELECT id FROM dietRestricts WHERE type='" + restriction + "';");
-    //   Bookshelf.knex.raw("INSERT INTO userDietRestricts (user_id, restriction_id) VALUES ('" + user.attributes.id + "', '" + restriction_id + "');");
-    // });
-    var userId = 1;
-    User
-    .forge({id: userId})
-    .fetch()
-    .then(function(user) {
-        //get the other model to add to the join table
-        return DietRestriction
-                .forge({type: 'vegan'})
-                .fetch()
-                .then(function(restriction) {
-                    //send both references down the promise chain
-                    return {dietRestrictionModel: restriction, userModel: user};
-                });
 
+  getDietRestrictions: function (user) {
+    return DietRestriction
+    .forge()
+    .fetch()
+    .then(function(restriction) {
+      return {dietRestrictionModel: restriction, userModel: user};
     })
     .then(function(references) {
-      console.log('references', references)
-        return references
-                .userModel
-                //get the belongsToMany relation specified in the first definition, which returns a collection
-                .dietRestrictions()
-                //attach the target to the collection, not the model instance
-                .attach(references.dietRestrictionModel);
+      return references
+        .userModel
+        .dietRestrictions()
+        .fetch();
     })
-    .then(function(relation) {
-        console.log('alskdfjlkasdjflkjsdflkjalskdj')
-    });
-    // new DietRestriction()  //{type: restriction}
-    // .query().where(...userRestrictions)
-    // .fetch()
-    // // .then(function(restriction){
-    //   // totalRestrictions.push(restriction);
-    // .then(function(restriction){
-    //   user.DietRestriction.dietRestrictions().attach(restriction);
-    // });
+   .then(function(relation) {
+      return relation.models.map(function(model) {
+        console.log('model atts.type', model.attributes.type);
+        return model.attributes.type;
+       });
+   });
   },
 
-  removeDietRestriction: function (user, userRestrictions) {
-    // new DietRestriction()
-    // .query().where
-    // .fetch()
-    // userRestrictions.forEach(function (restriction) {
-      // var removableRestriction_ids = bookshelf.knex.raw("SELECT id FROM dietRestricts WHERE type <> '" + restriction + "';"); // array of restrictions not being submitted?
-      // removableRestriction_ids.forEach(function (removableRestriction_id) {
-        Bookshelf.knex.raw("DELETE * FROM userDietRestricts WHERE user_id = '" + user.attributes.id + "';");
-      // });
-    // });
+  editDietRestrictions: function (next, user, newRestrictions) {
+    var toAdd = [];
+    var toRemove = [];
+    dietServices.getDietRestrictions(user)
+      .then(function(storedRestrictions) {
+        newRestrictions.forEach(function (current) {
+          if (storedRestrictions.indexOf(current) === -1) {
+            toAdd.push(current);
+          }
+        })
+        storedRestrictions.forEach(function (stored) {
+          if (newRestrictions.indexOf(stored) === -1) {
+            toRemove.push(stored);
+          }
+        });
+    })
+    .then(function(){
+      console.log(toAdd, toRemove);
+      dietServices.removeDietRestrictions(user, toRemove);
+    })
+    .then(function() {
+      dietServices.addDietRestrictions(user, toAdd);
+    })
+    .then(function () {
+      dietServices.getDietRestrictions(user);
+    })
+    .catch(function(error) {
+      console.log('Error in editing Diet Restrictions', error);
+    });
+  },
+
+  addDietRestrictions: function (user, toAdd) {
+    toAdd.forEach(function(item) {
+      add(item);
+    });
+    function add(item) {
+        return DietRestriction
+          .forge({type: item})
+          .fetch()
+          .then(function(restriction) {
+            return {dietRestrictionModel: restriction, userModel: user};
+          })
+      .then(function(references) {
+          return references
+            .userModel
+            .dietRestrictions()
+            .attach(references.dietRestrictionModel);
+      })
+      .then(function(relation) {
+          console.log('Successfully created relationship in addDietRestrictions function', relation);
+      }).catch(function(error){
+          return next(new Error('Failed to create relationship in addDietRestrictions function'))
+      });
+    };
+  },
+
+  removeDietRestrictions: function (user, toRemove) {
+    toRemove.forEach(function(item) {
+      remove(item);
+    });
+
+    function remove(item) {
+      return DietRestriction
+        .forge({type: item})
+        .fetch()
+        .then(function(restriction) {
+          return {dietRestrictionModel: restriction, userModel: user};
+        })
+        .then(function(references) {
+          console.log('references', references)
+          return references
+            .userModel
+            .dietRestrictions()
+            .detach(references.dietRestrictionModel);
+        })
+        .then(function(relation) {
+          console.log('Successfully removed relationship in removeDietRestrictions function', relation);
+        }).catch(function(error){
+          return next(new Error('Failed to remove relationship in removeDietRestrictions function' + error));
+        });
+    }
   }
 };
+// User.forge().fetch({id: 1}).then(function(user){
+//   dietServices.editDietRestrictions(null, user, ['vegetarian', 'vegan']);
+// });
 
-// dietServices.addDietRestriction();
