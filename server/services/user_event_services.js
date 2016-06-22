841044993f353ec2b1977c7072115e2847221481
Nate Blain <blain89@gmail.com>
2016-06-21 18:49:27 -0700

    [chore] start event interaction implementation

--- server/services/user_event_services.js
+++ server/services/user_event_services.js
@@ -1,11 +1,40 @@
-connectEventUser
+var Events = require('../data/collections/events');
+var Event = require('../data/models/events');
+var UserEvents = require('../data/collections/user_events');
+var UserEvent = require('../data/models/user_event');
+var Users = require('./../data/collections/users');
+var User = require('./../data/models/user');
 
-getUserEvents
+module.exports = UserEventServices = {
 
-getRecommendations
+  connectEventUsers: function (event, eventUsers /* array of users add to an event */) {
 
-getUserEventFoodPrefs
+  },
 
-getResponseStatus //used in getUserEvents
+  getUserEvents: function (user) {
+    return UserEvent
+      .forge({user_id: user.attributes.id})
+      .fetchAll();
+      .then(function (user) {
+        return Event
+          .forge({})
+      }
+  },
 
-submitUserEventPrefs
+  getRecommendations: function () {
+
+  }, //event model
+
+  getUserEventFoodPrefs: function () {
+
+  },
+
+  getResponseStatus: function () {
+
+  }, //used in getUserEvents
+
+  submitUserEventPrefs: function () {
+
+  }
+
+};
