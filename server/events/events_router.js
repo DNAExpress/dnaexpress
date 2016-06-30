8424cb1764a67603de6f5be03f53d14d2b2dd62e
Nate Blain <blain89@gmail.com>
2016-06-29 19:55:06 -0700

    [chore] add new endpoint for future decline events functionality

--- server/events/events_router.js
+++ server/events/events_router.js
@@ -1,8 +1,10 @@
 var eventsController = require('./events_controller');
+var UserEventServices = require('./../services/user_event_services');
 
 module.exports = function (app) {
   app.post('/create', eventsController.createEvent);
   app.post('/delete', eventsController.deleteEvent);
+  // app.post('/decline', UserEventServices.declineEvent);
   app.post('/formsubmission', eventsController.formSubmission);
   app.post('/getevents', eventsController.getUsersEvents);
   app.post('/selectrestaurant', eventsController.selectRestaurant);
