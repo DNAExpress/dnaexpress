e4f09de285dc706a044405af01e8bde18cc90328
Nate Blain <blain89@gmail.com>
2016-06-29 19:56:17 -0700

    [chore] promisify function and add missing parameter

--- server/search/search_controller.js
+++ server/search/search_controller.js
@@ -1,6 +1,9 @@
 var api_router = require('./../data/api_requests/api_router.js');
 var searchAlgorithm = require('./searchalgorithm');
 var Recommendation = require('./../data/models/recommendation');
+// var eventControls = require('./../events/events_controller');
+var MailServer = require('./../mail_server/mail_server');
+var User = require('./../data/models/user');
 
 module.exports = searchControls = {
 
@@ -25,16 +28,16 @@ module.exports = searchControls = {
     api_router.askYelp(searchCriteria, callback);
   },
 
-  getEventRecommendations: function (userAndEventDetails) {
+  getEventRecommendations: function (userAndEventDetails, event) {
     // sample userAndEventDetails: {location:'', restrictions: [], userFoodPrefs: [], eventId: int};
     var searchInput = {};
 
     searchInput.location = userAndEventDetails.location;
     var searchCriteria = searchAlgorithm.parseBestOptions(userAndEventDetails.userFoodPrefs);
     var dietRestriction = getUnique(userAndEventDetails.restrictions);
-    // make three calls to makeRequest with request details, (a food pref, diet restrictions, and location) 
+    // make three calls to makeRequest with request details, (a food pref, diet restrictions, and location)
     // pass in a callback to add the result to recommendations (this will need userAndEventDetails.eventId)
-    searchCriteria.forEach(function(criteria) {
+    Promise.all(searchCriteria.map(function(criteria) {
       var searchInput = {
         location: userAndEventDetails.location,
         term: dietRestriction.concat(criteria[0]).join(',')  // yelp search term requires a comma seperated / stringed list
@@ -44,10 +47,10 @@ module.exports = searchControls = {
       searchControls.makeRequest(searchInput, function(searchResults) {
         var topRecommendations = searchResults.slice(0, 4);
 
-        topRecommendations.forEach(function(recommendation) {
+        topRecommendations.map(function(recommendation) {
           // should create a seperate add recommendation function on the events controller...
           var newRecommendation  = {
-            event_id: userAndEventDetails.eventId, 
+            event_id: userAndEventDetails.eventId,
             name: recommendation.name,
             address: recommendation.location.address[0],
             city: recommendation.location.city,
@@ -55,19 +58,26 @@ module.exports = searchControls = {
             rating_img_url: recommendation.rating_img_url,
             snippet_image_url: recommendation.snippet_image_url,
             url: recommendation.url,
-            userVotes: userVotes
+            userVotes: userVotes,
+            image_url: recommendation.image_url
           };
           new Recommendation(newRecommendation).save().then(function(recom) {
-            console.log('new recommendation saved!')
+            console.log('new recommendation saved!');
           })
           .catch(function(error) {
             console.log(error);
-          })
-        })
+          });
+        });
+            // email event creator to alert them to choose a restaurant
       })
-    })
+      return 1;
+    }))
+    .then(function (n) {
+        console.log('How many times is this logging?');
+        emailCreator(event);
+    });
 
-    // helper function: 
+    // helper functions:
     function getUnique() {
       var n = [];
       for(var i = 0; i < this.length; i++) {
@@ -75,12 +85,21 @@ module.exports = searchControls = {
       }
       return n;
     };
+
+    function emailCreator(event) {
+      User
+       .forge({username: event.attributes.creator})
+       .fetch()
+       .then(function (user) {
+         return MailServer.mail(user.attributes.firstname, '/mail_Templates/creatorAlert.html', user.attributes.email, event.attributes.name);
+       });
+    };
   }
 
 }
 
 //sample current request body from client:
-    // search req body { 
+    // search req body {
     //  location: 'boston',
     //   opt1: 'indian'
     // }
